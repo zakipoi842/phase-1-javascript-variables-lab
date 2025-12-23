cc6ecfc950fc93e65ac5e20a744ed6bd75264ffe
zakaria <zakariyeali351@gmail.com>
2025-12-23 10:21:09 +0300

    javascript-variables-lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-
+console.log (companyName)
 let mostProfitableNeighborhood = 'Chelsea';
-
+console.log(mostProfitableNeighborhood)
 let companyCeo = 'Susan Smith';
+console.log(companyCeo)
